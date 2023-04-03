8f82133f068f3c150bed474b0483c3df2d2884f8
parsajiravand <parsajiravand@gmail.com>
2023-04-03 16:42:14 +0330

    update module

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import { pwaPlugin, unocssPlugin, unpluginComponents } from './.vitepress/build'
 
 export default defineConfig({
+  build: {
+    minify: false,
+  },
   plugins: [unpluginComponents, unocssPlugin, pwaPlugin],
   server: {
     fs: {
